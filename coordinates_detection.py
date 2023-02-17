d87a5beadd5598cd4d7c696d2ff454e729cffe9f
jw38176 <1300618628@qq.com>
2023-02-17 22:27:33 +0000

    Final detection code

--- coordinates_detection.py
+++ coordinates_detection.py
@@ -10,6 +10,7 @@ import json
 import time 
 import fcntl
 
+
 # Load the gesture recognizer models
 with open("/opt/lampp/htdocs/Gesture-Control/gesture_recognition_model.pickle", "rb") as target:
     trained_model = pickle.load(target)
@@ -17,31 +18,31 @@ print("\nLoaded model in gesture_recongnition_model.pickle\n")
 
 
 # Load class names
-label_map = {0: 'fist', 1: 'thumbs_down', 2: 'thumbs_up',
-             3: 'one', 4: 'two', 5: 'three', 6: 'fuck you dumbass'}
+label_map = {0: 'Fist', 1: 'Thumbs Down', 2: 'Thumbs Up', 3: 'One', 4: 'Two', 5: 'Three', 6: 'Rock', 7: 'Right', 8: 'Left', 9: 'Three', 10: 'Three'}
 
 #Initialize className 
 className = ""
 while True: 
+    className_previous = className
     with open('coordinates.json') as f:
         coordinates = json.load(f)
 
-    landmarks = np.array(coordinates)
-    reshaped_landmarks = landmarks.reshape(1, 63)
-    torch_landmarks = torch.from_numpy(reshaped_landmarks)
+    if coordinates == []: 
+        className = "Not Detected"
+    else: 
+        landmarks = np.array(coordinates)
+        reshaped_landmarks = landmarks.reshape(1, 63)
+        torch_landmarks = torch.from_numpy(reshaped_landmarks)
 
-    # Predict gesture
-    y_predicted = trained_model.model(torch_landmarks.float())
+        # Predict gesture
+        y_predicted = trained_model.model(torch_landmarks.float())
 
-    # print(prediction)
-    className_previous = className
-    classID = np.argmax(y_predicted.detach().numpy())
-    className = label_map[classID]
+        # print(prediction)
+        classID = np.argmax(y_predicted.detach().numpy())
+        className = label_map[classID]
 
     # Only write on change in label 
     if(className != className_previous): 
-
-
         with open("/opt/lampp/htdocs/dash/resources/prediction.txt", "w") as f:
             try:
                 fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -54,4 +55,3 @@ while True:
 
     # Add artificial slowdown to reduce CPU load (40% improvement)
     time.sleep(0.2)
-    
